bd318ba245fa40be6bdc3809b9396f2764ecf121
mdjamee306 <mdjamee400@gmail.com>
2023-09-16 15:22:27 +0600

    i have just added the carts.jsx in the courses.jsx files and add  flex , gap to identify them

---
+++ src/assets/Carts/Carts.jsx
@@ -0,0 +1,9 @@
+const Carts = () => {
+    return (
+        <div>
+            <h1 className="text-5xl">This is cart </h1>
+        </div>
+    );
+};
+
+export default Carts;
--- src/assets/Courses/Courses.jsx
+++ src/assets/Courses/Courses.jsx
@@ -1,36 +1,49 @@
 /* eslint-disable react/jsx-key */
 import { useEffect } from "react";
 import { useState } from "react";
+import Carts from "../Carts/Carts";
 
 const Courses = () => {
     const [courses, setCourses] = useState([]);
+    const [selectItem, setSelectItem] = useState([]);
     useEffect(() => {
         fetch('./data.json')
             .then(res => res.json())
             .then(data => setCourses(data))
     }, [])
+
+    // handle click 
+
+    const handleSelect = (courses) => {
+        setSelectItem([...selectItem,courses]);
+    }
+
     return (
-        <div className="container mx-auto ">
-                {/* parent of courses */}
+        <div className="container mx-auto flex gap-7">
+            {/* parent of courses */}
             <div className="grid grid-cols-3 gap-6 w-2/3 mb-20">
                 {
                     courses.map((courses) => (
-                        <div key={courses.id} className="shadow-2xl rounded p-4">
+                        <div key={courses.id} className="shadow-2xl rounded-xl p-4 ">
                             {/* child of course */}
                             <div className="">
                                 {/* img div */}
                                 <div><img className="w-full mt-4" src={courses.img} alt="" /></div>
                                 <h3 className="my-4 text-xl font-semibold">{courses.title}</h3>
                                 <p className="font-normal text-[#1C1B1B99] text-sm">{courses.description}</p>
-                                <div className="flex mt-4">
+                                <div className="flex gap-3 mt-4">
                                     <p>$ price: {courses.price}</p>
-                                    <p>$ credit: {courses.credit}</p>
+                                    <p>$ credit: {courses.credit}hr</p>
                                 </div>
-                                <button className="font-semibold text-xl text-white bg-[#2F80ED] px-28 py-2 rounded-lg mt-6 hover:bg-slate-300 hover:text-black">Select</button>
+                                <button onClick={() => handleSelect(courses)} className="font-semibold text-xl text-white bg-[#2F80ED] px-28 py-2 rounded-lg mt-6 hover:bg-slate-300 hover:text-black">Select</button>
                             </div>
                         </div>
                     ))
                 }
+            </div>
+            {/* cart */}
+            <div className="w-1/3">
+                <Carts></Carts>
             </div>
         </div>
     );
